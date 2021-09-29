a4bd898c085bb88f75bcbcc272ffe09cfc8489fc
garrettruss <garrettmruss@gmail.com>
2021-09-29 15:36:45 -0400

    interface

---
+++ 07-interfaces/Driver.ts
@@ -0,0 +1,15 @@
+import { Coach } from "./Coach";
+import { CricketCoach } from "./CricketCoach";
+import { GolfCoach } from "./GolfCoach";
+
+let myCricketCoach = new CricketCoach();
+let myGolfCoach = new GolfCoach();
+
+let theCoaches: Coach[] = [];
+
+theCoaches.push(myCricketCoach);
+theCoaches.push(myGolfCoach);
+
+for (let tempCoach of theCoaches) {
+    console.log(tempCoach.getDailyWorkout());
+}
